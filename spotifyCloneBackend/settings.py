aceb153cab41de7653a0fb563097f8cd74f624db
truong_nhat <nhattruong16062003@gmail.com>
2025-03-14 18:38:27 +0700

    fix init

--- spotifyCloneBackend/settings.py
+++ spotifyCloneBackend/settings.py
@@ -18,8 +18,8 @@ AUTH_USER_MODEL = 'models.User'  # Đăng ký custom User model
 
 INSTALLED_APPS = [
     # 'django.contrib.admin',
-    # 'django.contrib.auth',
-    # 'django.contrib.contenttypes',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
